536310c2713e41902532dce1d9a6f102c5899bfe
ValerieBecquart <r0809699@student.thomasmore.be>
2022-10-31 09:22:00 +0100

    integration test + extra getter to get data by level

--- src/main/java/fact/it/game_service/controller/GameRestController.java
+++ src/main/java/fact/it/game_service/controller/GameRestController.java
@@ -17,6 +17,7 @@ public class GameRestController {
 
     @PostConstruct
     public void fillDB(){
+
         if (gameRepository.count() == 0){
             gameRepository.save(new Game(1,"vraag 1", 1, 123.45, 567.89, "juist", "fout", "fout",4,4,"EXTRA_PotionVial"));
             gameRepository.save(new Game(2,"vraag 2", 1, 150, 50, "juist", "fout", "fout",5,0,"DEF_RoundShield"));
@@ -39,7 +40,11 @@ public class GameRestController {
         Game q = gameRepository.findGameByGameId(id);
         return q;
     }
-
+    @GetMapping("/questionsbylevel/{level}")
+    public List<Game> getAllQuestionsByLevel(@PathVariable int level){
+        List<Game> q = gameRepository.findAllByLevelIs(level);
+        return q;
+    }
     //POST: question
     @PostMapping("/question")
     public Game createQuestion(@RequestBody Game question){
